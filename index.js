a8e02d272b6587282f1badd17bc5a2d1c8a1367b
TRISTAN MICHAEL CORCORAN <tristancorcoran@gmail.com>
2019-08-01 17:53:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function dwarfRollCall(dwarves) {"Doc", "Dopey", "Bashful", "Grumpy"
 }
 var dwarves = 'dwarfRollCall' 
-dwarfRollCall; return ("1. Dopey 2. Grumpy 3. Bashful")
+return ("1. Dopey 2. Grumpy 3. Bashful")
 function summonCaptainPlanet(planeteerCalls){
 }
 
