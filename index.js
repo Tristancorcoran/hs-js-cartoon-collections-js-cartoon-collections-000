55fdeb423f86dec9cfc954003a20e0e60b44bf46
TRISTAN MICHAEL CORCORAN <tristancorcoran@gmail.com>
2019-08-01 18:07:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ let listDwarves = []
   }
 
 function summonCaptainPlanet(planeteerCalls){
+    let uppercasePlaneteerCalls = [];
+  for(let i = 0; i < planeteerCalls.length; i++){
+    let punctuation = "!"
+    uppercasePlaneteerCalls.push(`${planeteerCalls[i]}${punctuation}`.toUpperCase())
+  }let listDwarves = []
+  for(let i = 0; i < dwarves.length; i++){
+    
+    listDwarves.push(`${i+1}. ${dwarves[i]} `)
+    
+  }
+  return uppercasePlaneteerCalls
+}
 }
 
 function longPlaneteerCalls(words) {
