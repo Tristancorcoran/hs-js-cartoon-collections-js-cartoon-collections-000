00aaf0047ccf9c0690c7dbbc6ec6040673bf72bb
TRISTAN MICHAEL CORCORAN <tristancorcoran@gmail.com>
2019-08-01 18:08:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let listDwarves = []
   }
 
 function summonCaptainPlanet(planeteerCalls){
-    let uppercasePlaneteerCalls = [];
+  let uppercasePlaneteerCalls = [];
   for(let i = 0; i < planeteerCalls.length; i++){
     let punctuation = "!"
     uppercasePlaneteerCalls.push(`${planeteerCalls[i]}${punctuation}`.toUpperCase())
@@ -21,10 +21,29 @@ function summonCaptainPlanet(planeteerCalls){
   }
   return uppercasePlaneteerCalls
 }
-}
+
+
 
 function longPlaneteerCalls(words) {
+  
+  for(let i = 0; i < words.length; i++){
+    
+    if(words[i].length>4){
+      return true
+    }
+  }
+  return false
 }
 
+
+
 function findTheCheese (foods) {
-}
+  
+  for (var i = 0; i < foods.length; i++) {
+    
+    if (foods[i] ===  'gouda'|| foods[i]===  'cheddar' || foods[i]==='camembert') {
+      return foods[i];
+    }
+  }
+  return 'no cheese!'
+  
