6ffc284292ac0d22d1823416f4bd4fdfc4bba3f0
TRISTAN MICHAEL CORCORAN <tristancorcoran@gmail.com>
2019-08-01 18:00:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ function dwarfRollCall(dwarves) {
 let dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(Array)
 let i =  0;
-while i < dwarves.index
+while i < array.length 
+    puts "#{i + 1}. #{array[i]}"
+    i += 1 
 
 function summonCaptainPlanet(planeteerCalls){
 }
